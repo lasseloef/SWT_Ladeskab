e9618e31940676655f0f46b9409230de758f1bbf
Patrick Clausen <201811369@post.au.dk>
2020-10-25 17:22:27 +0100

    Added ChargeControl class

---
+++ Ladeskab/Ladeskab.Library/ChargeControl/ChargeControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab.Library.ChargeControl
+{
+    public class ChargeControl : IChargeControl
+    {
+        public IUsbCharger UsbCharger { get; set; }
+
+        public ChargeControl()
+        {
+        }
+
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler<ChargerEventArgs> ChargeEvent;
+    }
+}
