0dc23d40cb394537f173d918a1e481ea6a275e97
Patrick Clausen <201811369@post.au.dk>
2020-10-25 19:31:13 +0100

    Added tests for Door.LockDoor

---
+++ Ladeskab/Ladeskab.Unit.Tests/DoorUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ladeskab.Library.Door;
+using Microsoft.VisualStudio.TestPlatform;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Tests
+{
+    [TestFixture]
+    public class DoorUnitTests
+    {
+        private Door uut;
+
+        [SetUp]
+        public void DoorUnitTestsSetUp()
+        {
+            uut = new Door();
+        }
+
+        [Test]
+        public void LockDoor_CalledWithClosedDoor_DoorIsLocked()
+        {
+            //ARRANGE
+            uut.CloseDoor();
+
+            //ACT
+            uut.LockDoor();
+
+            //ASSERT
+            Assert.That(uut.Locked, Is.True);
+        }
+
+        [Test]
+        public void LockDoor_CalledWithOpenDoor_DoorIsNotLocked()
+        {
+            //ARRANGE
+            uut.OpenDoor();
+
+            //ACT
+            uut.LockDoor();
+
+            //ASSERT
+            Assert.That(uut.Locked, Is.False);
+        }
+    }
+}
