ea3b1a9b78f908a369f159307f3cc4e2816ecaf4
Patrick Clausen <201811369@post.au.dk>
2020-10-25 17:27:20 +0100

    Added ChargeControlUnitTests

---
+++ Ladeskab/Ladeskab.Unit.Tests/ChargeControlUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ladeskab.Library.ChargeControl;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Tests
+{
+    [TestFixture]
+    public class ChargeControlUnitTests
+    {
+        ChargeControl uut;
+        private IUsbCharger UsbChargerSubstitute;
+
+        [SetUp]
+        public void ChargeControlUnitTestsSetup()
+        {
+            UsbChargerSubstitute = Substitute.For<IUsbCharger>();
+            uut = new ChargeControl();
+        }
+
+        
+    }
+}
