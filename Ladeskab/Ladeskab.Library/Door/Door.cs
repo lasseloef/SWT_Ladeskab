ae567a4a3d8e0e6c2e83fd4191c2b7205ce7bec4
Patrick Clausen <201811369@post.au.dk>
2020-10-25 19:09:19 +0100

    Added Empty Door class

---
+++ Ladeskab/Ladeskab.Library/Door/Door.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab.Library.Door
+{
+    public class Door : IDoor
+    {
+        public event EventHandler DoorOpenedEvent;
+        public event EventHandler DoorClosedEvent;
+        public void LockDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnlockDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseDoor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
