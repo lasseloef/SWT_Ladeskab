fb0f344227902615eb84a4a0e779e117b015b375
Patrick Clausen <201811369@post.au.dk>
2020-10-26 23:11:05 +0100

    Added unit test for DoorOpenState.HandleOpenDoor

--- Ladeskab/Ladeskab.Unit.Tests/DoorOpenStateUnitTest.cs
+++ Ladeskab/Ladeskab.Unit.Tests/DoorOpenStateUnitTest.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Ladeskab.Library.StationControl;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Ladeskab.Unit.Tests
@@ -10,11 +12,26 @@ namespace Ladeskab.Unit.Tests
     public class DoorOpenStateUnitTest
     {
         private DoorOpenState uut;
+        private IControl controlSubstitute;
 
         [SetUp]
         public void DoorOpenStateUnitTestSetup()
         {
+            controlSubstitute = Substitute.For<IControl>();
             uut = new DoorOpenState();
+        }
+
+        [Test]
+        public void HandleOpenDoor_Called_stationControlReceivedNoCalls()
+        {
+            //ARRANGE
+            //Arrange step completed in setup
+
+            //ACT
+            uut.HandleOpenDoor(controlSubstitute);
+
+            //ASSERT
+            Assert.That(controlSubstitute.ReceivedCalls().Count(), Is.EqualTo(0));
         }
     }
 }
