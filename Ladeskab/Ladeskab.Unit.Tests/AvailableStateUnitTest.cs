4e6367c18b0028d8c5903e0817c3f9f73ed76091
Patrick Clausen <201811369@post.au.dk>
2020-10-27 00:24:39 +0100

    Added testfile for AvailableState

---
+++ Ladeskab/Ladeskab.Unit.Tests/AvailableStateUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ladeskab.Library.ChargeControl;
+using Ladeskab.Library.StationControl;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Tests
+{
+    [TestFixture]
+    public class AvailableStateUnitTest
+    {
+        private AvailableState uut;
+        private IControl controlSubstitute;
+
+        [SetUp]
+        public void LockedStateUnitTestSetup()
+        {
+            controlSubstitute = Substitute.For<IControl>();
+            uut = new AvailableState();
+        }
+
+    }
+}
