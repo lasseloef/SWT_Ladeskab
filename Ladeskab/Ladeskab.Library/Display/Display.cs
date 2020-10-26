1ca53cdec013db0bc70a76426ccab82e72ec2ded
HamannMojn <55832937+HamannMojn@users.noreply.github.com>
2020-10-26 22:30:23 +0100

    Implemented display  unittest

---
+++ Ladeskab/Ladeskab.Library/Display/Display.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab.Library.Display
+{
+    public class Display : IDisplay
+    {
+        public void DisplayMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
