50fe282357fa5e20a60e3d22d44ff3cb18fda96c
Patrick Clausen <201811369@post.au.dk>
2020-10-26 23:44:25 +0100

    Implemented LockedState.HandleRfid

--- Ladeskab/Ladeskab.Library/StationControl/LockedState.cs
+++ Ladeskab/Ladeskab.Library/StationControl/LockedState.cs
@@ -17,7 +17,18 @@ namespace Ladeskab.Library.StationControl
 
         public void HandleRfid(IControl stationControl, int id)
         {
-
+            if (id != stationControl.OldId)
+            {
+                stationControl.Disp.DisplayMessage("ERROR: RFID doesn't match!");
+            }
+            else
+            {
+                stationControl.ChargeControl.StopCharge();
+                stationControl.Door.UnlockDoor();
+                stationControl.Logger.LogDoorUnlocked(id);
+                stationControl.Disp.DisplayMessage("Door unlocked, please remove phone");
+                stationControl.SetState(stationControl.Available);
+            }
         }
 
         public void HandleCharge(IControl stationControl, ChargerEventArgs args)
