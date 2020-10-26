bc9f4cc5ff148a32d56bd135961c3cdaf89d4926
Patrick Clausen <201811369@post.au.dk>
2020-10-26 23:35:40 +0100

    Added test for LockedState.HandleOpenDoor

---
+++ Ladeskab/Ladeskab.Unit.Tests/LockedStateUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ladeskab.Library.StationControl;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Tests
+{
+    [TestFixture]
+    public class LockedStateUnitTest
+
+    {
+        private LockedState uut;
+        private IControl controlSubstitute;
+
+        [SetUp]
+        public void LockedStateUnitTestSetup()
+        {
+            controlSubstitute = Substitute.For<IControl>();
+            uut = new LockedState();
+        }
+
+        [Test]
+        public void HandleOpenDoor_Called_stationControlDispDisplaysCorrectMessage()
+        {
+            //ARRANGE
+            //Arrange step completed in setup
+
+            //ACT
+            uut.HandleOpenDoor(controlSubstitute);
+
+            //ASSERT
+            controlSubstitute.Disp.Received().DisplayMessage("ERROR: Door is locked!");
+        }
+    }
+}
