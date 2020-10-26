dde907e5dfbd37864b1338be9a337648573fb4d4
Patrick Clausen <201811369@post.au.dk>
2020-10-27 00:46:46 +0100

    Added unit testing for AvailableState.HandleRfid

--- Ladeskab/Ladeskab.Unit.Tests/AvailableStateUnitTest.cs
+++ Ladeskab/Ladeskab.Unit.Tests/AvailableStateUnitTest.cs
@@ -88,5 +88,148 @@ namespace Ladeskab.Unit.Tests
             //Assert that substitute received no calls
             Assert.That(controlSubstitute.ReceivedCalls().Count(), Is.EqualTo(0));
         }
+
+        [Test]
+        public void HandleRfid_CalledWithDisconnectedPhone_DisplaysCorrectMessage()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(false);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.Disp.Received().DisplayMessage("ERROR: Phone not connected");
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithDisconnectedPhone_IdIsNotSaved()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(false);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            Assert.That(controlSubstitute.OldId, Is.Not.EqualTo(12345));
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithDisconnectedPhone_DoorIsNotLocked()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(false);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.Door.DidNotReceive().LockDoor();
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithDisconnectedPhone_DoesntStartCharging()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(false);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.ChargeControl.DidNotReceive().StartCharge();
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithDisconnectedPhone_LoggerNotCalled()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(false);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.Logger.DidNotReceiveWithAnyArgs().LogDoorLocked(default);
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithDisconnectedPhone_SetStateNotCalled()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(false);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.DidNotReceiveWithAnyArgs().SetState(default);
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithConnectedPhone_IdIsSaved()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(true);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            Assert.That(controlSubstitute.OldId, Is.EqualTo(12345));
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithConnectedPhone_DoorIsLocked()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(true);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.Door.Received().LockDoor();
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithConnectedPhone_StartsCharging()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(true);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.ChargeControl.Received().StartCharge();
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithConnectedPhone_LoggerLogdoorLockedCalledWithID()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(true);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.Logger.Received().LogDoorLocked(12345);
+        }
+
+        [Test]
+        public void HandleRfid_CalledWithConnectedPhone_SetStateToLocked()
+        {
+            //ARRANGE
+            controlSubstitute.ChargeControl.IsConnected().Returns(true);
+
+            //ACT
+            uut.HandleRfid(controlSubstitute, 12345);
+
+            //ASSERT
+            controlSubstitute.Received().SetState(controlSubstitute.Locked);
+        }
     }
 }
