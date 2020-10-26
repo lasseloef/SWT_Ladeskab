561f1749aa1d8212fea1d9188ec7a16687e89947
HamannMojn <55832937+HamannMojn@users.noreply.github.com>
2020-10-26 23:05:23 +0100

    implemented logger with unittests

---
+++ Ladeskab/Ladeskab.Library/Logger/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab.Library.Logger
+{
+    public class Logger : ILogger
+    {
+        public void LogDoorLocked(int id)
+        {
+            Console.WriteLine("Door locked by:" + id);
+        }
+
+        public void LogDoorUnlocked(int id)
+        {
+            Console.WriteLine("Door unlocked by:" + id);
+        }
+    }
+}
