dc112247d4bf8a30a00038b723c67acfcaca9f66
Patrick Clausen <201811369@post.au.dk>
2020-10-28 21:27:07 +0100

    Added TestFixture for ConnectedPhoneState

---
+++ Ladeskab/Ladeskab.Unit.Tests/ConnectedPhoneStateUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ladeskab.Library.ChargeControl;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Tests
+{
+    [TestFixture]
+    public class ConnectedPhoneStateUnitTest
+    {
+        private ConnectedPhoneState uut;
+        [SetUp]
+        public void ConnectedPhoneStateUnitTestSetUp()
+        {
+            uut = new ConnectedPhoneState();
+        }
+    }
+}
